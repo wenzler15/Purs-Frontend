62012ae6446860733171ca789da683834dcac889
Henrique Dias <henriquedias_28@hotmail.com>
2023-05-18 13:19:53 -0300

    update chart page to call component

--- src/pages/ChartPage/index.tsx
+++ src/pages/ChartPage/index.tsx
@@ -1,32 +1,88 @@
-import React from 'react';
-import Header from '../../Components/Header';
-import { AiOutlineDownload } from 'react-icons/ai';
-import NavBar from '../../Components/NavBar';
+import React from "react";
+import Header from "../../Components/Header";
+import {
+  AiOutlineDownload,
+} from "react-icons/ai";
+import NavBar from "../../Components/NavBar";
+
+import OrgChart from "../../Components/OrgChart";
+
+type RowInterface = {
+  name: string;
+  title: string;
+  email: string;
+  father?: string;
+  children?: Array<RowInterface>;
+};
+
+/*
+  Load data from excel
+  Use the father field to indicate the e-mail of highest hierarchical node
+ */
+const dataList = [
+  { name: "Tamara Santos", title: "Designer", email: "tamara.santos@purs" },
+  {
+    name: "João Alves",
+    title: "UI Designer",
+    email: "joao.alvez@purs",
+    father: "tamara.santos@purs",
+  },
+  {
+    name: "Ana Clara",
+    title: "Estagiária",
+    email: "ana.clara@purs",
+    father: "joao.alvez@purs",
+  },
+  {
+    name: "Patricia Silva",
+    title: "Estagiária",
+    email: "patricia.silva@purs",
+    father: "ana.clara@purs",
+  },
+  {
+    name: "Maria Soares",
+    title: "UI Designer",
+    email: "maria.soares@purs",
+    father: "tamara.santos@purs",
+  },
+  {
+    name: "Angela Nunes",
+    title: "ADM",
+    email: "angela.nunes@purs",
+    father: "tamara.santos@purs",
+  },
+  {
+    name: "Micaelly",
+    title: "Aux Adm",
+    email: "micaely@purs",
+    father: "angela.nunes@purs",
+  },
+] as RowInterface[];
 
 const ChartPage: React.FC = () => {
-    return (
-        <div className='flex h-screen overflow-hidden'>
-            <div className='w-1/6'>
-                <NavBar />
+  return (
+    <div className="flex h-screen overflow-hidden">
+      <div className="w-1/6">
+        <NavBar />
+      </div>
+      <div className="w-full">
+        <Header />
+        <div className="w-full bg-[#E4ECF5] h-full pt-10 pl-4 pr-4 box-border">
+          <div className="flex">
+            <div className="flex w-[200] bg-purple-purs p-2 rounded-md cursor-pointer">
+              <AiOutlineDownload size={20} color={"#fff"} />
+              <p className="text-[#fff] ml-2 text-sm"> Exportar</p>
             </div>
-            <div className='w-full'>
-                <Header />
-                <div className='w-full bg-[#E4ECF5] h-full pt-10 pl-4 pr-4 box-border'>
-                    <div className='flex'>
-                        <div className='flex w-[200] bg-purple-purs p-2 rounded-md cursor-pointer'>
-                            <AiOutlineDownload size={20} color={'#fff'} />
-                            <p className='text-[#fff] ml-2 text-sm'> Exportar</p>
-                        </div>
-                    </div>
-                    <div className='w-full h-full mt-10'>
-                        <div className='bg-[#fff] w-full h-4/5 rounded-md'>
-
-                        </div>
-                    </div>
-                </div>
+          </div>
+          <div className="w-full h-full mt-10">
+            <div className="bg-[#fff] w-full h-4/5 rounded-md">
+              <OrgChart datalist={dataList} />
             </div>
+          </div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default ChartPage;
