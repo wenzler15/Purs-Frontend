43cda78ea14038f2acea1f5796bc12a3715a4628
Douglas Wenzler Santos <douglas@MacBook-Air-de-Douglas.local>
2023-08-02 09:15:46 -0300

    responsible

--- src/pages/LP/index.tsx
+++ src/pages/LP/index.tsx
@@ -84,7 +84,7 @@ const LP: React.FC = () => {
 
     return (
         <div className='w-full'>
-            <div className='h-[80vh] border w-full bg-woman bg-cover min-h-[760px] sm:h-[200%]' id="principal">
+            <div className='pb-6 w-full bg-woman bg-cover min-h-[760px]' id="principal">
                 <div className='flex flex-row justify-between p-10'>
                     <img src={Logo} className='w-[50] ' />
                     <div className='flex flex-row justify-between w-[40%]'>
@@ -95,53 +95,55 @@ const LP: React.FC = () => {
                     </div>
                 </div>
                 <div className='flex justify-between flex-wrap pr-10'>
-                    <div className='w-[35%] pl-10 mt-32 sm:w-full'>
+                    <div className='w-full min-w-[250px] mb-10 pl-10 mt-2 sm:mt-32 sm:w-[35%]'>
                         <p className='text-[#fff] font-bold text-2xl mb-2'>Descomplicamos a gestão de pessoas unindo tecnologia e humanização</p>
                         <p className='text-[#fff] text-2xl'>pensando em torná-la estratégica e simples.</p>
                     </div>
-                    <div className='w-[25%] min-w-[300px] bg-[#fff] rounded-md p-5 text-center shadow-lg'>
-                        <p className='text-[#3E79A5] text-2xl'>Fale com um especialista</p>
-                        <div className='w-full flex flex-col items-center text-left'>
-                            <div className='w-[90%] mt-4'>
-                                <p className='text-[#3E79A5]'>Nome *</p>
-                                <input placeholder='Digite seu nome' onChange={(e) => {
-                                        setNameError(false); 
-                                        setName(e.target.value)
-                                    }} className='mt-3 border rounded-md p-2 w-full' />
-                                {nameError && (
-                                    <p className='text-[#f00] text-sm'>*Favor preencher o nome</p>
-                                )}
+                    <div className='w-full sm:w-[25%] min-w-[300px] flex justify-center'>
+                        <div className='w-[80%] sm:w-full bg-[#fff] rounded-md p-5 text-center shadow-lg'>
+                            <p className='text-[#3E79A5] text-2xl'>Fale com um especialista</p>
+                            <div className='w-full flex flex-col items-center text-left'>
+                                <div className='w-[90%] mt-4'>
+                                    <p className='text-[#3E79A5]'>Nome *</p>
+                                    <input placeholder='Digite seu nome' onChange={(e) => {
+                                            setNameError(false); 
+                                            setName(e.target.value)
+                                        }} className='mt-3 border rounded-md p-2 w-full' />
+                                    {nameError && (
+                                        <p className='text-[#f00] text-sm'>*Favor preencher o nome</p>
+                                    )}
 
-                                <p className='text-[#3E79A5] mt-4 '>E-mail *</p>
-                                <input placeholder='Digite ser e-mail' onChange={(e) => {
-                                        setEmailError(false)
-                                        setEmail(e.target.value)
-                                    }} className='mt-3  border rounded-md p-2 w-full' />
-                                {emailError && (
-                                    <p className='text-[#f00] text-sm'>*Favor preencher com um e-mail valido</p>
-                                )}
+                                    <p className='text-[#3E79A5] mt-4 '>E-mail *</p>
+                                    <input placeholder='Digite ser e-mail' onChange={(e) => {
+                                            setEmailError(false)
+                                            setEmail(e.target.value)
+                                        }} className='mt-3  border rounded-md p-2 w-full' />
+                                    {emailError && (
+                                        <p className='text-[#f00] text-sm'>*Favor preencher com um e-mail valido</p>
+                                    )}
 
-                                <p className='text-[#3E79A5] mt-4'>Empresa *</p>
-                                <input placeholder='Digite o nome da sua empresa' onChange={(e) => {
-                                        setCompanyError(false)
-                                        setCompany(e.target.value)
-                                    }} className='mt-3 border rounded-md p-2 w-full' />
-                                {companyError && (
-                                    <p className='text-[#f00] text-sm'>*Favor preencher o nome da empresa</p>
-                                )}
+                                    <p className='text-[#3E79A5] mt-4'>Empresa *</p>
+                                    <input placeholder='Digite o nome da sua empresa' onChange={(e) => {
+                                            setCompanyError(false)
+                                            setCompany(e.target.value)
+                                        }} className='mt-3 border rounded-md p-2 w-full' />
+                                    {companyError && (
+                                        <p className='text-[#f00] text-sm'>*Favor preencher o nome da empresa</p>
+                                    )}
 
-                                <p className='text-[#3E79A5] mt-4'>Telefone *</p>
-                                <input maxLength={15} onChange={(event) => {
-                                        setPhoneError(false)
-                                        handlePhone(event)
-                                    }} placeholder='(00) 0000-0000' className='mt-3 border rounded-md p-2 w-full' />
-                                {phoneError && (
-                                    <p className='text-[#f00] text-sm'>*Favor preencher o telefone</p>
-                                )}
+                                    <p className='text-[#3E79A5] mt-4'>Telefone *</p>
+                                    <input maxLength={15} onChange={(event) => {
+                                            setPhoneError(false)
+                                            handlePhone(event)
+                                        }} placeholder='(00) 0000-0000' className='mt-3 border rounded-md p-2 w-full' />
+                                    {phoneError && (
+                                        <p className='text-[#f00] text-sm'>*Favor preencher o telefone</p>
+                                    )}
 
+                                </div>
                             </div>
+                            <button onClick={() => sendEmail()} className='bg-[#331A71] text-[#fff] p-3 rounded-lg mt-4'>Falar com um especialista</button>
                         </div>
-                        <button onClick={() => sendEmail()} className='bg-[#331A71] text-[#fff] p-3 rounded-lg mt-4'>Falar com um especialista</button>
                     </div>
                 </div>
             </div>
@@ -149,35 +151,35 @@ const LP: React.FC = () => {
                 <p className='text-[#fff] text-3xl'>A Purs chega em...</p>
                 <div className='flex w-full justify-center mt-6'>
                     <div className='mr-10'>
-                        <p className='text-8xl text-[#fff]'>10</p>
-                        <p className='text-2xl text-[#fff]'>Dias</p>
+                        <p className='sm:text-8xl text-5xl text-[#fff]'>10</p>
+                        <p className='sm:text-2xl text-2xl text-[#fff]'>Dias</p>
                     </div>
                     <div className='mr-10'>
-                        <p className='text-8xl text-[#fff]'>:</p>
+                        <p className='sm:text-8xl text-5xl text-[#fff]'>:</p>
                     </div>
                     <div className='mr-10'>
-                        <p className='text-8xl text-[#fff]'>10</p>
-                        <p className='text-2xl text-[#fff]'>Horas</p>
+                        <p className='sm:text-8xl text-5xl text-[#fff]'>10</p>
+                        <p className='sm:text-2xl text-2xl text-[#fff]'>Dias</p>
                     </div>
                     <div className='mr-10'>
-                        <p className='text-8xl text-[#fff]'>:</p>
+                        <p className='sm:text-8xl text-5xl text-[#fff]'>:</p>
                     </div>
                     <div className='mr-10'>
-                        <p className='text-8xl text-[#fff]'>10</p>
-                        <p className='text-2xl text-[#fff]'>Minutos</p>
+                        <p className='sm:text-8xl text-5xl text-[#fff]'>10</p>
+                        <p className='sm:text-2xl text-2xl text-[#fff]'>Dias</p>
                     </div>
                 </div>
                 <a href='#principal'>
                     <button onClick={() => sendEmail()} className='bg-[#331A71] text-[#fff] p-3 rounded-lg mt-10'>Falar com um especialista</button>
                 </a>
             </div>
-            <div className='w-full pt-10 bg-[#5B359E] flex justify-between'>
-                <div className='relative w-[50%] flex justify-center'>
-                    <img src={Woman} className='absolute top-[12%] z-10 w-[60%] max-w-[400px]' />
-                    <img src={RectangleComplete} className='relative w-[90%] h-[500px] max-w-[400px]' />
+            <div className='w-full pt-10 bg-[#5B359E] flex justify-between flex-wrap'>
+                <div className='relative sm:w-[50%] sm:min-w-[310px] flex justify-center'>
+                    <img src={Woman} className='absolute top-[12%] z-10 w-[65%] sm:w-[60%] sm:max-w-[400px]' />
+                    <img src={RectangleComplete} className='relative w-[90%] h-[500px] sm:max-w-[400px]' />
                 </div>
-                <div className='w-[50%] text-center mt-[5%]'>
-                    <div className='w-[90%] text-left'>
+                <div className='w-full sm:w-[50%] text-center mt-[5%] pb-5 pl-5'>
+                    <div className='text-center w-[90%] sm:text-left'>
                         <p className='text-[#B8E6F6] text-2xl'>Quem somos?</p>
                         <p className='text-[#fff] mt-10'>Somos uma empresa de tecnologia focada em descomplicar a gestão de pessoas e torná-la estratégica e simples, fortalecendo líderes para focar no que realmente importa: pessoas!</p>
                         <p className='text-[#fff] mt-6'>Acreditamos que uma gestão estratégica de pessoas, humanizada e baseada em dados, é fundamental para alcançar resultados positivos.</p>
@@ -187,7 +189,7 @@ const LP: React.FC = () => {
                     </a>
                 </div>
             </div>
-            <div className='w-full pt-10 bg-[#5B359E] flex justify-between pb-10'>
+            <div className='w-full pt-10 bg-[#5B359E] flex justify-between pb-10 flex-wrap-reverse'>
                 <div className='w-[70%] text-center mt-[5%] pl-10'>
                     <div className='w-[90%] text-left'>
                         <p className='text-[#B8E6F6] text-2xl'>Por que escolher a Purs?</p>
@@ -212,7 +214,7 @@ const LP: React.FC = () => {
                         <button onClick={() => sendEmail()} className='bg-[#331A71] text-[#fff] p-3 rounded-lg mt-6'>Falar com um especialista</button>
                     </a>
                 </div>
-                <div className='relative w-[50%]'>
+                <div className='relative w-[50%] min-w-[310]'>
                     <img src={Macbook} className='absolute top-[20%] ml-[-10px] z-10 w-[80%] h-[65%] max-w-[400px] sm:w-[98%]' />
                     <img src={RectangleComplete} className='top-[14%] relative w-[70%] max-w-[400px] sm:w-[90%]' />
                 </div>
